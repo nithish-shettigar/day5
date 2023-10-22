55001190304999080cebb27c0e01ed421d8b4220
nithish-shettigar <129670979+nithish-shettigar@users.noreply.github.com>
2023-10-22 21:53:36 +0530

    Add files via upload

---
+++ Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function Signup(){
+    return(
+        <>
+            <h1>Signup Page</h1>
+        </>
+    )
+    
+} 
+export default Signup;
